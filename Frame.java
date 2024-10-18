e27db2bc0a29f448cbabbe99084cbb0bbd05e14a
TonySanchez27 <91620554+TonySanchez27@users.noreply.github.com>
2024-10-18 00:03:29 -0400

    Getting rid of unnecessary bulk

--- Frame.java
+++ Frame.java
@@ -1,3 +1,12 @@
+/**
+* Car Payment Calculator
+*
+* Visualizes what a car will cost to purchase initially and
+* monthly.
+*
+* @author Tony Sanchez October 2024
+*/
+
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
@@ -5,29 +14,28 @@ import javax.swing.*;
 
 public class Frame extends JFrame{
 
-  JPanel newPanel, subPanel;
-  //innerPanel() littlePanel;
+  JPanel newPanel, subPanel1, subPanel2;
+
   Frame(){
     newPanel = new JPanel();
-    subPanel = new JPanel();
-    //littlePanel = new innerPanel();
-    subPanel.setBackground(Color.GREEN);
+    subPanel1 = new JPanel();
+    subPanel2 = new JPanel();
+    subPanel1.setBackground(Color.GREEN);
     newPanel.setBackground(Color.RED);
     newPanel.setSize(200, 300);
-    //subPanel.setBorder(BorderFactory.createEmptyBorder(100, 20, 20, 20)); // Top, Left, Bottom, Right
+    subPanel1.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20)); // Top, Left, Bottom, Right
     newPanel.setBorder(BorderFactory.createTitledBorder("Main Panel"));
     newPanel.setLayout(new GridLayout(1,2));
-    subPanel.setBorder(BorderFactory.createTitledBorder("Sub Panel 1"));
-    subPanel.setLayout(new GridLayout(1,1));
+    subPanel1.setBorder(BorderFactory.createTitledBorder("Sub Panel 1"));
+    subPanel1.setLayout(new GridLayout(1,1));
+    subPanel2.setBorder(BorderFactory.createTitledBorder("Sub Panel 2"));
+    subPanel2.setLayout(new GridLayout(1,1));
 
-    this.add(subPanel);
+    newPanel.add(subPanel1);
+    newPanel.add(subPanel2);
     this.add(newPanel);
-    subPanel.setSize(300, 400);
-    //newPanel.add(subPanel);
-    //subPanel.setBorder(BorderFactory.createTitledBorder("Sub Panel 1"));
-    //subPanel.setLayout(new GridLayout(1,1));
-    //subPanel.setBackground(Color.blue);
-    //subPanel.setVisible(true);
+    subPanel1.setSize(10, 400);
+    
     this.setSize(900, 500);
     this.setTitle("Car Payment Calculator");
     this.setResizable(true);
